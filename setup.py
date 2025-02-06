14ba30b0e9a44a5a749cb223c74d32b24bd53620
manar-c <manar.chammas@gmail.com>
2025-02-05 21:53:00 -0600

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 from setuptools import setup
 
-#Include in environment for testing:  pip install -e .
-#This needs to be run in the directory before this
 
 setup(
     name="pyDataconverter",
-    version="0.1",
+    version="0.01",
     packages=['pyDataconverter'],
     python_requires='>=3.7',
     install_requires=[
-        # Add any dependencies your package needs here
+        'numpy',
+        'scipy',
+        'matplotlib'
     ],
     author="Manar El-Chammas",
-    description="A Python data converter toolbox",
+    description="A Python toolbox for modeling and analyzing data converters",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/pyDataconverter",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License ::  GPL-3.0 license ",
+        "Operating System :: OS Independent",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "Development Status :: 3 - Alpha",
