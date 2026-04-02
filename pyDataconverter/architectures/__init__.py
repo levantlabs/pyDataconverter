a00f8784c29da942bb3b64ee8f228c44f99b8511
Manar El-Chammas <manar.chammas@gmail.com>
2026-04-02 14:11:25 -0500

    feat: export new SAR/CDAC classes from package __init__.py

---
+++ pyDataconverter/architectures/__init__.py
@@ -0,0 +1,5 @@
+from .SARADC import SARADC, MultibitSARADC, NoiseshapingSARADC
+from .FlashADC import FlashADC
+from .SimpleADC import SimpleADC
+from .SimpleDAC import SimpleDAC
+from .CurrentSteeringDAC import CurrentSteeringDAC
--- pyDataconverter/components/__init__.py
+++ pyDataconverter/components/__init__.py
@@ -3,3 +3,11 @@ from .reference import ReferenceBase, ReferenceLadder, ArbitraryReference
 from .decoder import DecoderBase, BinaryDecoder, ThermometerDecoder, SegmentedDecoder
 from .capacitor import UnitCapacitorBase, IdealCapacitor
 from .current_source import UnitCurrentSourceBase, IdealCurrentSource, CurrentSourceArray
+from .cdac import (
+    CDACBase,
+    SingleEndedCDAC,
+    DifferentialCDAC,
+    RedundantSARCDAC,
+    SplitCapCDAC,
+    SegmentedCDAC,
+)
